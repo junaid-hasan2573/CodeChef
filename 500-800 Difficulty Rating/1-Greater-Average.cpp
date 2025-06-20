4dfe78ebe7cc3673ca20f4cee44ab64871a97758
Junaid Hasan <junaidhasa2002@gmail.com>
2025-06-20 22:14:13 +0600

    Update 1-Greater-Average.cpp

--- 500-800 Difficulty Rating/1-Greater-Average.cpp
+++ 500-800 Difficulty Rating/1-Greater-Average.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, t;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a >> b >> c;
+        cout << ((((a + b) / 2.00) > c) ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
