e089627afa0bae9f025d62a02d1d147af997f922
Junaid Hasan <junaidhasa2002@gmail.com>
2025-06-21 23:27:04 +0600

    Create 1-Cricket-World-Cup-Qualifier.cpp

---
+++ 500 Difficulty Rating/1-Cricket-World-Cup-Qualifier.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    if (x >= 12)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+    return 0;
+}
