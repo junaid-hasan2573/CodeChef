20c86a54c667c9402c07e9f6cb80779d02f85d65
Junaid Hasan <junaidhasa2002@gmail.com>
2025-07-02 23:24:02 +0600

    Create 12-Off-By-One.cpp

---
+++ 500 Difficulty Rating/12-Off-By-One.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << (a + b) << "1" << endl;
+    return 0;
+}
+
+/*  [Best Solution]
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << 10 * (a + b) + 1 << endl;
+}*/
