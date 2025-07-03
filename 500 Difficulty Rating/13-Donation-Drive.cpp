6eae8c7aaf8f77cc6f8cd1c50ba72f87243d2d14
Junaid Hasan <junaidhasa2002@gmail.com>
2025-07-03 22:58:35 +0600

    Create 13-Donation-Drive.cpp

---
+++ 500 Difficulty Rating/13-Donation-Drive.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, n, t;
+    cin >> t;
+    while (t--)
+    {
+        cin >> x >> n;
+        cout << (x - n) << endl;
+    }
+    return 0;
+}
