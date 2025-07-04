7dae6bcf2ef98188b705b04b44568a3e7abddc51
Junaid Hasan <junaidhasa2002@gmail.com>
2025-07-04 19:15:10 +0600

    Create 14-Kitchen-Timings.cpp

---
+++ 500 Difficulty Rating/14-Kitchen-Timings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y, t;
+    cin >> t;
+    while (t--)
+    {
+        cin >> x >> y;
+        cout << (y - x) << endl;
+    }
+    return 0;
+}
