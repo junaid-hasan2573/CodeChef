df55f46ce0ec6723e7aab1e0f16a2a0ee4b28507
Junaid Hasan <junaidhasa2002@gmail.com>
2025-07-17 23:23:26 +0600

    Create 27-Parity.cpp

---
+++ 500 Difficulty Rating/27-Parity.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, t;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        if ((n % 2) == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
