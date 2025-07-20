41e18f28efec6cc025e4ff2535d8ff5b53e55b41
Junaid Hasan <junaidhasa2002@gmail.com>
2025-07-20 21:13:11 +0600

    Create 30-Battery-Health.cpp

---
+++ 500 Difficulty Rating/30-Battery-Health.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, t;
+    cin >> t;
+    while (t--)
+    {
+        cin >> x;
+        if (x >= 80)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
